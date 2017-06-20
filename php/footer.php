5bfc0498c225253d407c4cd708adfe04e42a8302
AndreeaBila <andreeabila@yahoo.com>
2017-06-21 00:47:07 +0300

    footer

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,41 @@
 <footer>
-  
+  <div class="col-md-4 footer-component text-center">
+    <h2>List Manager &trade;</h2>
+    <ul class="socials">
+      <li><a href="#"><img src="../img/twitter.png" alt="Not Available"></a></li>
+      <li><a href="#"><img src="../img/googleplus.png" alt="Not Available"></a></li>
+      <li><a href="#"><img src="../img/facebook.png" alt="Not Available"></a></li>
+    </ul>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </div>
+
+  <div class="col-md-4 footer-component text-center">
+    <h3>Contact us</h3>
+    <p><i class="fa fa-envelope" aria-hidden="true"></i> listmanager@gmail.com</p>
+    <p><i class="fa fa-map-marker" aria-hidden="true"></i> Studio M, 6th Floor,</p>
+    <p>Victoriei Street, Bucharest</p>
+    <p><i class="fa fa-phone" aria-hidden="true"></i> +40 723 538 293</p>
+  </div>
+
+  <div class="col-md-4 footer-component text-center">
+    <h3><i class="fa fa-newspaper-o" aria-hidden="true"> Subscribe</i></h3>
+    <p>Subscribe to our newsletter to stay up to date</p>
+    <form class="form-inline">
+      <div class="form-group">
+          <label for="emailNewsletter"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i></label>
+          <input type="email" class="form-control" id="emailNewsletter" placeholder="Email">
+          <input type="submit" name="" value="Subscribe">
+     </div>
+    </form>
+  </div>
+
+  <div class="clear"></div>
+  <hr>
+  <div class="col-md-6">
+    <p>Created by &copy; Andreea & Vlad </p>
+  </div>
+
+  <div class="col-md-6 below-footer">
+    <p class="pull-right"><i class="fa fa-cc-visa fa-2x" aria-hidden="true"></i> <i class="fa fa-cc-paypal fa-2x" aria-hidden="true"></i> <i class="fa fa-cc-mastercard fa-2x" aria-hidden="true"></i></p>
+  </div>
 </footer>
